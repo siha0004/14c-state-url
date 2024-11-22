a3cb46ecec9d543861bc2f0404dd8cc7cef3f8a0
Sif Melkane <siha0004@stud.kea.dk>
2024-11-22 14:11:41 +0100

    fixed with forms - not styled

--- src/app/page.js
+++ src/app/page.js
@@ -26,32 +26,36 @@ export default function Home() {
     <main className="m-5">
       <section className="grid gap-6 md:grid-cols-2">
         <Image src={tshirt} alt="product image"></Image>
-        <article className="flex flex-col gap-3">
+        <form action="/payment" className="flex flex-col gap-3">
           <h2 className="text-3xl font-bold">T-shirt</h2>
-          <h3>Size</h3>
-          <ul className="flex gap-3">
-            {size.map((size) => {
-              return (
-                <li key={size} className={`${searchParams.get("size") == size && "bg-slate-100"} p-2 border hover:bg-slate-100`}>
-                  <Link href={`?${createQueryString("size", size)}`}>{size}</Link>
-                </li>
-              );
-            })}
-          </ul>
-          <h3>Color</h3>
-          <ul className="flex gap-3">
-            {color.map((color) => {
-              return (
-                <li key={color} className={`${searchParams.get("color") == color && "bg-slate-100"} p-2 border hover:bg-slate-100`}>
-                  <Link href={`?${createQueryString("color", color)}`}>{color}</Link>
-                </li>
-              );
-            })}
-          </ul>
-          <Link href={`/payment/?${searchParams}`} className="bg-green-800 text-white px-16 py-3 mt-4 inline-block w-fit">
-            Buy
-          </Link>
-        </article>
+          <fieldset>
+            <legend>Size</legend>
+            <div className="flex gap-3">
+              {size.map((size) => {
+                return (
+                  <label for={size} className="p-2 border hover:bg-slate-100">
+                    <input id={size} type="radio" name="size" value={size} />
+                    {size}
+                  </label>
+                );
+              })}
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>Color</legend>
+            <div className="flex gap-3">
+              {color.map((color) => {
+                return (
+                  <label for={color} className="p-2 border hover:bg-slate-100">
+                    <input id={color} type="radio" name="color" value={color} />
+                    {color}
+                  </label>
+                );
+              })}
+            </div>
+          </fieldset>
+          <button className="bg-green-800 text-white px-16 py-3 mt-4 inline-block w-fit">Buy</button>
+        </form>
       </section>
     </main>
   );
